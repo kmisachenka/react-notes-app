9a58fdc6add2ed0fb38b2dc991378d5c1fc4b147
kmisachenka <kmisachenka@gmail.com>
2016-11-08 22:15:30 +0300

    Add note editor and ability to add new note

--- notes.js
+++ notes.js
@@ -8,9 +8,35 @@ const Note = React.createClass({
 });
 
 const NoteEditor = React.createClass({
+    getInitialState: function () {
+        return {
+            text: ''
+        }
+    },
+    handleTextChange: function (event) {
+        this.setState({ text: event.target.value });
+    },
+    handleNoteAdd: function () {
+        const newNote = {
+            text: this.state.text,
+            color: 'yellow',
+            id: Date.now()
+        };
+
+        this.props.onNoteAdd(newNote);
+    },
     render: function () {
         return (
-            <div className="note-editor"> NoteEditor </div>
+            <div className="note-editor">
+                <textarea
+                    placeholder="Enter your note here..."
+                    rows={5}
+                    className="textarea"
+                    value={this.state.text}
+                    onChange={this.handleTextChange}
+                />
+                <button className="add-button" onClick={this.handleNoteAdd}>Add</button>
+            </div>
         );
     }
 });
@@ -98,12 +124,17 @@ const NotesApp = React.createClass({
 
         };
     },
+    handleNoteAdd: function (newNote) {
+        const newNotes = this.state.notes.slice();
+        newNotes.unshift(newNote);
+        this.setState({ notes: newNotes });
+    },
     render: function () {
         return (
             <div className="notes-app">
-            NotesApp
-            <NoteEditor/>
-            <NotesGrid notes={this.state.notes}/>
+                <h2 className="app-header">NotesApp</h2>
+                <NoteEditor onNoteAdd={this.handleNoteAdd}/>
+                <NotesGrid notes={this.state.notes}/>
             </div>
         );
     }
