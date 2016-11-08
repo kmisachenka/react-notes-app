e6d4ba479f1c29b3547fee4552b06c92eb5a0e0e
kmisachenka <kmisachenka@gmail.com>
2016-11-08 22:01:01 +0300

    Add styling notes layout

---
+++ notes.js
@@ -0,0 +1,115 @@
+const Note = React.createClass({
+    render: function () {
+        const style = { backgroundColor: this.props.color };
+        return (
+            <div className="note" style={style}> {this.props.children} </div>
+        );
+    }
+});
+
+const NoteEditor = React.createClass({
+    render: function () {
+        return (
+            <div className="note-editor"> NoteEditor </div>
+        );
+    }
+});
+
+const NotesGrid = React.createClass({
+    componentDidMount: function () {
+        const grid = this.refs.grid;
+        const msnry = new Masonry(grid, {
+            itemSelector: '.note',
+            columnWidth: 200,
+            gutter: 10
+        });
+    },
+    render: function () {
+        return (
+            <div className="notes-grid" ref="grid">
+                {
+                    this.props.notes.map(function (note) {
+                        return <Note key={note.id} color={note.color} > {note.text} </Note>
+                    })
+                }
+            </div>
+        );
+    }
+});
+
+const NotesApp = React.createClass({
+    getInitialState: function () {
+        return {
+            notes: [
+                {
+                    id: 0,
+                    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut scelerisque.",
+                    color: "#FFD700"
+
+                },
+                {
+                    id: 1,
+                    text: "Ivamus venenatis cursus ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel dictum urna.",
+                    color: "#20B2AA"
+
+                },
+                {
+                    id: 2,
+                    text: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. " +
+                    "Proin venenatis rutrum lobortis. Nulla facilisi. Aliquam sed fermentum eros, et consectetur erat. " +
+                    "Sed egestas diam erat, a commodo nibh venenatis in. Nam id consectetur quam, vitae sollicitudin enim.",
+                    color: "#1AD2FF"
+
+                },
+                {
+                    id: 3,
+                    text: "Sed sed lacus ultricies, accumsan odio pellentesque, auctor mi. Mauris maximus nunc nec lectus " +
+                    "bibendum, rutrum scelerisque metus mattis. Class aptent taciti sociosqu ad litora torquent per conubia " +
+                    "nostra, per inceptos himenaeos. Pellentesque sodales mauris tellus, quis convallis massa tempus et.",
+                    color: "#4072AB"
+
+                },
+                {
+                    id: 4,
+                    text: "Ut quis euismod sapien. Sed ac pellentesque purus. Maecenas malesuada lobortis felis eu " +
+                    "imperdiet. Nam rutrum facilisis erat, eget congue augue dictum non. Nulla volutpat quam a elit interdum " +
+                    "tincidunt. Nam semper massa enim, at mattis nunc convallis in.",
+                    color: "#2FFF12"
+
+                },
+                {
+                    id: 5,
+                    text: "Fusce sed leo rutrum, viverra ante ac, sollicitudin erat. Vestibulum eu cursus erat. Duis " +
+                    "vestibulum sit amet arcu feugiat iaculis. Cras rutrum consectetur maximus.",
+                    color: "#FF12AD"
+
+                },
+                {
+                    id: 6,
+                    text: "Nullam aliquam enim a elit interdum dignissim. Curabitur fermentum ut dolor a pharetra. Morbi " +
+                    "metus dolor, sodales sit amet arcu nec, vehicula congue quam. Pellentesque nec egestas est, vitae " +
+                    "dignissim orci. Nullam est sapien, ultrices nec tincidunt quis, cursus at ante. ",
+                    color: "#F012FA"
+
+                }
+
+
+            ]
+
+        };
+    },
+    render: function () {
+        return (
+            <div className="notes-app">
+            NotesApp
+            <NoteEditor/>
+            <NotesGrid notes={this.state.notes}/>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+<NotesApp />,
+    document.getElementById("mount-point")
+);
